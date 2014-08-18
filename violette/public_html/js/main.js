bdd12bd93cc6d588380e282d8b17bafb9056a850
Chthonios <arschthonius@gmail.com>
2014-08-18 04:44:15 -0400

    modif référence pour localhost

--- violette/public_html/js/main.js
+++ violette/public_html/js/main.js
@@ -10,19 +10,19 @@ function envoyerEmploye(id) {
 	passwd.value = hash;
 	requeteServeur();
 }
-
+/*le url ici fait référence au localhost, si tout le projet est sur localhost pas besoin de CORS*/
 function requeteServeur() {
 	var passwd = document.getElementById("passwd");
 	hash = Sha1.hash(passwd.value);
 	passwd.value = hash;
 	alert(passwd.value);
 	$.ajax({
-		url: 'https://github.com/nissaba/violette/tree/gh-pages/violette/public_html/login.php',
+		url: 'http://127.0.0.1/projects/projet_final/login.php',
 		type: 'POST',
 		async: false,
 		data: $('#login_form').serialize(),
 		datatype: 'text',
-		success: function(response) { return alert('success : '+response.value);},
+		success: function(response) { return alert('success : '+response.getElementsByTagName("result_code") [0].nodeValue);},
 		error: function(response) { return alert('error : '+response.value);},
 		complete: function(response) { return alert('complete : '+response.value);}
     });
